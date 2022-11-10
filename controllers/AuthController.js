960a7e69f156721d06ae853c2af57230a674831e
Pedro Rambaldi <rambaldipedro@gmail.com>
2022-11-10 13:39:27 -0300

    finalizando autenticação

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -7,6 +7,39 @@ module.exports = class AuthController {
     res.render("auth/login");
   }
 
+  static async loginPost(req, res) {
+
+    const {email, password} = req.body
+
+    // FIND USER
+    const user = await User.findOne({where: {email: email}})
+
+    if(!user) {
+      req.flash("message", "Usuário não encontrado!");
+      res.render("auth/login");
+
+      return;
+    }
+
+    // CHECK IF PASSWORD MATCH
+    const passwordMatch = bcrypt.compareSync(password, user.password)
+
+    if(!passwordMatch) {
+      req.flash("message", "Senha inválida!");
+      res.render("auth/login");
+
+      return;
+    }
+    // INITIALIZE SESSION
+    req.session.userid = user.id;
+
+    req.flash("message", "Autenticação realizada com sucesso!");
+
+    req.session.save(() => {
+      res.redirect("/");
+    });
+  }
+
   static register(req, res) {
     res.render("auth/register");
   }
