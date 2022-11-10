b7ad327ee036c0342e2595e65cb8a63e2ebb9b38
Pedro Rambaldi <rambaldipedro@gmail.com>
2022-11-10 14:05:53 -0300

    middleware de verificação de autenticação

--- controllers/ToughtController.js
+++ controllers/ToughtController.js
@@ -5,4 +5,7 @@ module.exports = class ToughtCotroller {
     static async showThoughts(req, res) {
         res.render('toughts/home')
     }
+    static async dashboard(req, res) {
+        res.render('toughts/dashboard')
+    }
 }
--- routes/toughtsRoutes.js
+++ routes/toughtsRoutes.js
@@ -1,7 +1,11 @@
-const express = require('express')
-const router = express.Router()
-const ToughtController = require('../controllers/ToughtController')
+const express = require("express");
+const router = express.Router();
+const ToughtController = require("../controllers/ToughtController");
 
-router.get('/', ToughtController.showThoughts)
+// HELPERS
+const checkAuth = require("../helpers/auth").checkAuth;
 
-module.exports = router
+router.get("/dashboard", checkAuth, ToughtController.dashboard);
+router.get("/", ToughtController.showThoughts);
+
+module.exports = router;
